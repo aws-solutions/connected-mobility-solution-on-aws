da892e23c5052e9011acbe2b5e6e19151be0e5ce
Matt Wise <wsmatth@amazon.com>
2025-02-26 01:48:36 +0900

    Version v2.0.5 Release

--- source/lib/setup.py
+++ source/lib/setup.py
@@ -52,7 +52,7 @@ setup(
         "toml>=0.10.2",
     ],
     name="cms_common",
-    version="2.0.4",
+    version="2.0.5",
     description="Common library used in CMS modules",
     packages=find_packages(
         exclude=[
